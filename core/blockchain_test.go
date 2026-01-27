8e69c7054110ac79c6cb6c7b760ff886147ca4cd
w33ked <otieno.phidel11@gmail.com>
2026-01-27 13:29:07 +0300

    blockchain struct and block validation

---
+++ core/blockchain_test.go
@@ -0,0 +1,25 @@
+package core
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func newBlockchainWithGenesis(t *testing.T) *Blockchain {
+	bc, err := NewBlockchain(randomBlock(0))
+	assert.Nil(t, err)
+
+	return bc
+}
+
+func TestNewBlockchain(t *testing.T) {
+	bc := newBlockchainWithGenesis(t)
+	assert.NotNil(t, bc.validator)
+	assert.Equal(t, bc.Height(), uint32(0))
+}
+
+func TestHasBlock(t *testing.T) {
+	bc := newBlockchainWithGenesis(t)
+	assert.True(t, bc.HasBlock(0))
+}
