70cd7af05c239436f2dc104971a24f75274925f2
w33ked <otieno.phidel11@gmail.com>
2026-02-04 13:24:39 +0300

    fix empty logger in blockchain test

--- core/blockchain_test.go
+++ core/blockchain_test.go
@@ -3,6 +3,7 @@ package core
 import (
 	"testing"
 
+	"github.com/go-kit/log"
 	"github.com/stretchr/testify/assert"
 	"github.com/w33ked/theblockchain/types"
 )
@@ -55,7 +56,7 @@ func TestGetHeader(t *testing.T) {
 }
 
 func newBlockchainWithGenesis(t *testing.T) *Blockchain {
-	bc, err := NewBlockchain(randomBlock(t, 0, types.Hash{}))
+	bc, err := NewBlockchain(log.NewNopLogger(), randomBlock(t, 0, types.Hash{}))
 	assert.Nil(t, err)
 
 	return bc
